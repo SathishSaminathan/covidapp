9488427fc59018f99fd51287a42bbee184ce7132
SathishSaminathan <Sathishcse1975@gmail.com>
2020-04-18 00:05:16 +0530

    commit for dropdown

--- src/screens/CheckByCountry.js
+++ src/screens/CheckByCountry.js
@@ -2,13 +2,15 @@ import React, {Component, useEffect, useState} from 'react';
 import {Text, View, ScrollView, TouchableOpacity} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {startCase} from 'lodash';
+import RNPickerSelect from 'react-native-picker-select';
 
 import {Colors} from '../constants/ThemeConstants';
 import {heightPerc} from '../helpers/styleHelper';
 import TextComponent from '../components/Shared/TextComponent';
-import {FontType} from '../constants/AppConstants';
+import {FontType, IconType} from '../constants/AppConstants';
 import Axios from 'axios';
 import {currencyFormat} from '../helpers/validationHelper';
+import IconComponent from '../components/Shared/IconComponent';
 
 const names = ['INDIA', 'OTHER'];
 
@@ -40,6 +42,7 @@ const India = () => {
         borderRadius: 10,
         padding: 10,
         height: '55%',
+        elevation: 10,
         // alignItems: 'center',
       }}>
       <TextComponent style={{fontSize: 20}} type={FontType.BOLD}>
@@ -48,7 +51,7 @@ const India = () => {
       {State && (
         <View>
           {res.map((v, i) => (
-            <View style={{paddingVertical: 15}}>
+            <View style={{paddingVertical: 15}} key={i}>
               <TextComponent style={{fontSize: 15}} type={FontType.BOLD}>
                 {startCase(v)}
               </TextComponent>
@@ -66,20 +69,123 @@ const India = () => {
 };
 
 const Other = () => {
-  const [Countries, setCountries] = useState([]);
+  const [Countries, setCountries] = useState(null);
+  const [Country, setCountry] = useState(null);
+  const [State, setState] = useState();
   useEffect(() => {
     getCountries();
   }, []);
 
-  const getCountries = () => {
+  const getCountries = async () => {
     const {
       data: {countries},
-    } = axios.get('https://covid19.mathdro.id/api/countries');
-    setCountries(countries.map((country) => country.name));
+    } = await Axios.get('https://covid19.mathdro.id/api/countries');
+
+    setCountries(
+      countries.map((country) => {
+        return {label: country.name, value: country.name};
+      }),
+    );
+  };
+
+  const handleChange = (value) => {
+    getData(value);
+    setCountry(value);
+  };
+
+  const getData = (country) => {
+    Axios.get(`https://covid19.mathdro.id/api/countries/${country}`)
+      .then((res) => {
+        setState({
+          confirmed: res.data.confirmed.value,
+          recovered: res.data.recovered.value,
+          deaths: res.data.deaths.value,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
-    <View>
-      <TextComponent>Other</TextComponent>
+    <View
+      style={{
+        width: '90%',
+        height: '55%',
+        borderRadius: 10,
+      }}>
+      {/* <TextComponent>Other</TextComponent> */}
+      <View style={{backgroundColor: Colors.white, marginBottom: 10}}>
+        {Countries && (
+          <RNPickerSelect
+            placeholder={{
+              label: 'Select country',
+              value: null,
+              color: '#9EA0A4',
+            }}
+            items={Countries}
+            onValueChange={(value) => {
+              handleChange(value);
+            }}
+            useNativeAndroidPickerStyle={false}
+            // InputAccessoryView={() => null}
+            style={{
+              inputAndroid: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+                overflow: 'hidden',
+              },
+              iconContainer: {
+                top: '30%',
+                right: 15,
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+            }}
+            // value={this.state.favSport2}
+            Icon={() => {
+              return (
+                <IconComponent
+                  type={IconType.AntDesign}
+                  size={20}
+                  color="gray"
+                  name="down"
+                />
+              );
+            }}
+          />
+        )}
+      </View>
+      <View
+        style={{
+          height: '100%',
+          backgroundColor: Colors.white,
+          borderRadius: 10,
+          padding: 10,
+          elevation: 10,
+        }}>
+        <TextComponent style={{fontSize: 20}} type={FontType.BOLD}>
+          {Country && `Across ${Country}`}
+        </TextComponent>
+        {State && (
+          <View>
+            {res.map((v, i) => (
+              <View style={{paddingVertical: 15}} key={i}>
+                <TextComponent style={{fontSize: 15}} type={FontType.BOLD}>
+                  {startCase(v)}
+                </TextComponent>
+                <TextComponent
+                  style={{fontSize: 30, paddingVertical: 5, color: Colors[v]}}
+                  type={FontType.BOLD}>
+                  {currencyFormat(State[v])}
+                </TextComponent>
+              </View>
+            ))}
+          </View>
+        )}
+      </View>
     </View>
   );
 };
@@ -88,7 +194,7 @@ export default class CheckByCountry extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeMenu: names[0],
+      activeMenu: names[1],
     };
   }
 
